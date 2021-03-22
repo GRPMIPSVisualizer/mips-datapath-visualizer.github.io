66fc0101495de90ba264385618d0aae1eed27638
scykn1 <scykn1@nottingham.edu.cn>
2021-03-22 15:28:11 +0800

    deleting unnecessary file

--- js/Assembler/ts/List.ts
+++
@@ -1,9 +0,0 @@
-export interface List<E> {
-    add(object: E): void;                         
-    add(index: number, object: E): void;           
-    remove(index: number): boolean;           
-    remove(object: E): boolean;               
-    get(index: number): Object;               
-    size(): Number;                           
-    update(index: number, Object: E): void;   
-}
